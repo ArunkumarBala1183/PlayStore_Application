8ed5d83dbe6d9e19472b80ccddb8b01dd46eefdb
Arunkumar Bala <arunkumar.bala@aspiresys.com>
2024-02-19 18:42:25 +0530

    List of AppInfo updated to User Model

---
+++ Playstore Application/Playstore.Migrations/Migrations/20240219131016_Updated_List_of_AppInfo_to_Users.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Playstore.Migrations.Migrations
+{
+    public partial class Updated_List_of_AppInfo_to_Users : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppInfo_UserId",
+                table: "AppInfo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppInfo_UserId",
+                table: "AppInfo",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppInfo_UserId",
+                table: "AppInfo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppInfo_UserId",
+                table: "AppInfo",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
