77c254f187272c406fec56a9e1a5c87ab1d7187a
Arunkumar Bala <arunkumar.bala@aspiresys.com>
2024-03-06 20:15:56 +0530

    Integrated Login and Admin

--- Angular/Angular/src/app/app-routing.module.ts
+++ Angular/Angular/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './home/homepage/homepage.component';
-import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 import { AdminComponent } from './admin/admin.component';
-import { UsersComponent } from './admin/users/users.component';
-import { MyDownloadsComponent } from './admin/my-downloads/my-downloads.component';
-import { CategoryComponent } from './admin/category/category.component';
-import { LogsComponent } from './admin/logs/logs.component';
-import { ProfileComponent } from './admin/profile/profile.component';
 
 const routes: Routes = [
   {
--- Angular/Angular/src/app/app.module.ts
+++ Angular/Angular/src/app/app.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { AdminModule } from './admin/admin.module';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ToastrModule } from 'ngx-toastr';
-
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +12,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginModule } from './login/login.module';
 import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HomeModule } from './home/home.module';
-import { FormsModule } from '@angular/forms';
 import { JwtInterceptor } from './jwt.interceptor';
 import { JwtModule } from '@auth0/angular-jwt';
 import { ToastrModule } from 'ngx-toastr';
@@ -42,13 +32,13 @@ import { ToastrModule } from 'ngx-toastr';
     FormsModule,
     MatButtonModule,
     MatIconModule,
-    ToastrModule.forRoot()
     LoginModule,
     HomeModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: () => localStorage.getItem('token')
       }}),
+      ToastrModule.forRoot()
   ],
   providers: [
     {
