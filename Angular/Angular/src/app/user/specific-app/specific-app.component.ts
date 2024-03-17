2b16547cd2d3e5a4e7835c17b12e31f8535d9110
Arunkumar-siva <arunkumar.siva@aspiresys.com>
2024-03-17 22:46:16 +0530

    appdownload changes

--- Angular/Angular/src/app/user/specific-app/specific-app.component.ts
+++ Angular/Angular/src/app/user/specific-app/specific-app.component.ts
@@ -90,8 +90,9 @@ export class SpecificAppComponent implements OnInit {
   }
 
   public Downloadfile() {
-    this.route.params.subscribe((params) => {
-      const appId: Guid = params['appId'];
+    // this.route.params.subscribe((params) => {
+    //   const appId: Guid = params['appId'];
+    const appId=this.service.getAppId();
       const userId = this.loginService.getUserId();
       this.service.PostAppFile(appId, userId).subscribe({
         next: (response) => {
@@ -115,13 +116,14 @@ export class SpecificAppComponent implements OnInit {
           }
         },
       });
-    });
+    
   }
 
   public onSubmit() {
     if (this.reviewForm.valid) {
       const formData = this.reviewForm.value;
-      const appId = this.route.snapshot.params['appId'];
+      // const appId = this.route.snapshot.params['appId'];
+      const appId=this.service.getAppId();
       const userId = this.loginService.getUserId();
       formData.appId = appId;
       formData.userId = userId;
